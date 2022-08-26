405f0536f7cf65c9045da5eebfec114a6fea3446
Brendo Oliveira <brendo.dev@outlook.com>
2022-08-26 14:38:27 -0300

    update version

--- themes/welcome/home.php
+++ themes/welcome/home.php
@@ -6,7 +6,7 @@
 <div class="container mt-5 text-center ">
     <i class="bi bi-cup-hot-fill" style="font-size: 2em; color:blue"></i>
     <h2 class="logo mb-3 text-white">COFFE CODE</h2>
-    <p class="text-white">version Coffe Code 1.2 - PHP version: 7.4.29</p>
+    <p class="text-white">version Coffe Code 1.3 - PHP version: 7.4.29</p>
 
 </div>
 
